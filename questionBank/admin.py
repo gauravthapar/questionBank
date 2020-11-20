25d51efcd36fe8be073a5343b0d6ca8d6a222998
Gaurav Thapar <gauravthapar34@gmail.com>
2020-11-20 21:18:54 +0530

    admin: enhanced admin pannel for paper details model. added list display for list of objects, search field for searching and filters.

--- questionBank/admin.py
+++ questionBank/admin.py
@@ -2,7 +2,13 @@ from django.contrib import admin
 from . models import Student, QuestionPaperDetail, QuestionPaper, Feedback
 # Register your models here.
 
+class QuestionPaperDetailAdmin(admin.ModelAdmin):
+    list_display = ("id", "subjectName", "subjectCode", "year", "examType")
+    search_fields = ("subjectName", "subjectCode", "year")
+    list_filter = ("examType", "year",)
+    list_display_links = ("subjectName",)
+
 admin.site.register(Student)
 admin.site.register(QuestionPaper)
-admin.site.register(QuestionPaperDetail)
+admin.site.register(QuestionPaperDetail, QuestionPaperDetailAdmin)
 admin.site.register(Feedback)
