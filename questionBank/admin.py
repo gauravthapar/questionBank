3ce804630fe353c0a8d6b2bc42c489532191c7c0
Gaurav Thapar <gauravthapar34@gmail.com>
2020-11-21 14:40:13 +0530

    admin: enhanced admin pannel for question paper model. added list display, search field, filters.

--- questionBank/admin.py
+++ questionBank/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from . models import Student, QuestionPaperDetail, QuestionPaper, Feedback
+import os
 # Register your models here.
 
 class QuestionPaperDetailAdmin(admin.ModelAdmin):
@@ -8,7 +9,32 @@ class QuestionPaperDetailAdmin(admin.ModelAdmin):
     list_filter = ("examType", "year",)
     list_display_links = ("subjectName",)
 
+class QuestionPaperAdmin(admin.ModelAdmin):
+    list_display = ("details_id", "subjectName", "subjectCode", "year", "examType", "file_extension")
+    list_select_related = ("details",)
+    list_filter = ("details__examType", "details__year",)
+    search_fields = ("details__subjectName", "details__subjectCode", "details__year")
+    list_display_links = ("details_id","subjectName",)
+
+    def details_id(self, object):
+        return object.details.id
+
+    def subjectName(self, object):
+        return object.details.subjectName
+    
+    def subjectCode(self, object):
+        return object.details.subjectCode
+    
+    def year(self, object):
+        return object.details.year
+
+    def examType(self, object):
+        return object.details.examType
+    
+    def file_extension(self, object):
+        return os.path.splitext(object.File.name)[1]  
+
 admin.site.register(Student)
-admin.site.register(QuestionPaper)
+admin.site.register(QuestionPaper, QuestionPaperAdmin)
 admin.site.register(QuestionPaperDetail, QuestionPaperDetailAdmin)
 admin.site.register(Feedback)
